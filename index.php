2df879eb843c125feb0b59b01a8ea81ad18e8c60
Carlos Veliz de Villa <cvvaecc@gmail.com>
2021-11-24 19:01:51 -0500

    Interfaz index

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php 
+	session_start();
+	require_once 'conexion.php';
+	if (isset($_SESSION['user_id'])) {
+		$sql = $conn->prepare('SELECT id, email, password FROM users WHERE id = :id')
+		$sql -> bindParam(":id",$_SESSION['user_id']);
+		$sql -> execute();
+		$result = $sql->fetch(PDO::FETCH_ASSOC);
+		$user = null;
+		if (count($result) > 0) {
+			$user = $result;
+		}
+	}
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Welcome</title>
+</head>
+<body>
+	<header>
+		<a href="/php_login">Login del Sistema</a>
+	</header>
+	<?php if (!empty($user)):?>
+		<br>Welcome <?= $user['email']; ?>
+		<br>You are successfully Logged In
+		<a href="logout.php">Logout</a>
+	<?php else: ?>
+		<h1>Please Login or SignUP</h1>
+		<a href="login.php">Login</a>Or
+		<a href="singup.php">SignUP</a>
+	<?php endif; ?>
+</body>
+</html>
